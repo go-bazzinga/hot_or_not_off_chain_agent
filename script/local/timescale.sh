34653b8bd438f6a8f1f48efdeae60dba8bb40b65
Saikat Das <saikatdas0790@gmail.com>
2023-09-04 21:43:58 +0530

    fix: push unified timestamp from API instead of default from DB

--- script/local/timescale.sh
+++ script/local/timescale.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 docker run --detach --name timescaledb --publish 5432:5432 --env POSTGRES_PASSWORD=password --rm timescale/timescaledb:latest-pg15
 
-# sqlx migrate run
+sqlx migrate run
--- src/metrics.rs
+++ src/metrics.rs
@@ -1,14 +1,18 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use axum::{body::Bytes, extract::State, http::StatusCode};
 use candid::Principal;
+use chrono::{DateTime, NaiveDateTime, Utc};
 use serde::Deserialize;
 use sqlx::PgPool;
 
 #[derive(Deserialize, Debug)]
 pub struct CanisterStatus {
     pub canister_id: Principal,
-    cycle_balance: i64,
-    idle_cycles_burned_per_day: i64,
-    memory_size: i64,
+    pub cycle_balance: i64,
+    pub idle_cycles_burned_per_day: i64,
+    pub memory_size: i64,
+    pub timestamp: SystemTime,
 }
 
 #[derive(Deserialize, Debug)]
@@ -53,15 +57,25 @@ async fn insert_value_to_database(
     canister_status: CanisterStatus,
     pool: State<PgPool>,
 ) -> Result<(), StatusCode> {
+    let microseconds_since_unix_epoch = canister_status
+        .timestamp
+        .duration_since(UNIX_EPOCH)
+        .expect("Failed to get microseconds from canister timestamp")
+        .as_micros() as i64;
+    let naive_date_time = NaiveDateTime::from_timestamp_micros(microseconds_since_unix_epoch)
+        .expect("Failed to convert microseconds to NaiveDateTime");
+    let timestamp_date_time = DateTime::<Utc>::from_naive_utc_and_offset(naive_date_time, Utc);
+
     sqlx::query!(
         r#"
-        insert into canister_metrics (canister_id, cycle_balance, idle_cycles_burned_per_day, memory_size)
-        VALUES ($1, $2, $3, $4)
+        insert into canister_metrics (canister_id, cycle_balance, idle_cycles_burned_per_day, memory_size, recorded_at)
+        VALUES ($1, $2, $3, $4, $5)
         "#,
         canister_status.canister_id.to_text(),
         canister_status.cycle_balance,
         canister_status.idle_cycles_burned_per_day,
-        canister_status.memory_size
+        canister_status.memory_size,
+        timestamp_date_time
     ).execute(&*pool).await.map_err(|e| {
         println!("🛑 Failed to insert metrics: {:?}", e);
         StatusCode::INTERNAL_SERVER_ERROR
